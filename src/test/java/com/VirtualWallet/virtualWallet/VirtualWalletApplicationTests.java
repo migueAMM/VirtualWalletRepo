3ff570fe6f50c8100085f9e0740d900361592851
Miguel Angel Marin Montes <mamarinmontes@gmail.com>
2025-11-19 21:34:49 -0500

    Primer commit enlazando el repositorio

---
+++ src/test/java/com/VirtualWallet/virtualWallet/VirtualWalletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.VirtualWallet.virtualWallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualWalletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
